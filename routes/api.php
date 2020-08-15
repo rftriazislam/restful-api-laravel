e249208f0b611e4c03f83dba066606ee858620d0
riazislam09999 <riazislam09999@gmail.com>
2020-08-15 23:40:24 +0600

    auth api create

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/person',function(){
+    $person=[
+        "name"=>'riaz',
+        "age"=>'22'
+    ];
+    return $person;
+});
